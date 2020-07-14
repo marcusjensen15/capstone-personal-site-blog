a559bced4b8550f75ffb7d374fbb1641070f580b
Marcus Jensen <marcusjensen1515@gmail.com>
2020-07-14 11:28:53 -0700

    revise resume section to include code for pdx. remove epicodus experience

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -13,7 +13,7 @@ export default function Resume(){
         <div className="column">
 
           <h4 className="sectionHeader">PROFILE</h4>
-          <p className="listFont">Highly committed, and accomplished SaaS technology sales professional. In 2019, pivoted from SaaS Account Executive role and began intensively studying Software Engineering. Perpetually curious lifelong learner.</p>
+          <p className="listFont">I was a highly committed and accomplished SaaS technology sales professional. In 2019, pivoted from SaaS Account Executive role and began intensively studying Software Engineering to expand on Computer Science Minor. Perpetually curious lifelong learner.</p>
           <h4 className="sectionHeader">PROFESSIONAL SKILLS</h4>
             <div className="listFont">
               <p>JavaScript/ReactJS</p>
@@ -56,23 +56,24 @@ export default function Resume(){
 
             <h5>Theorem Agency</h5>
             <p className="italic">JavaScript/React Software Engineering Intern, 2020</p>
-            <p>Work collaboratively on a team with three other Software Engineering Interns. We have completed Front End work for clients like: The LA Galaxy, Aflac Insurance, and an HR Portal for Theorem to use internally. We work alongside Theorem's Senior Engineers.</p>
+            <p>Work collaboratively with the Software Engineering Team. I have completed Front End work for clients like: The LA Galaxy, Aflac Insurance, and built an HR Portal for Theorem to use internally.</p>
 
             <p>Received open ended project requirements. Before building anything, I spend time asking clarifying questions and eliminating ambiguity between myself and those defining project requirements. Saved time with this approach.</p>
 
 
-            <h5>Epicodus</h5>
-            <p className="italic">Full Time Student 2019-2020</p>
-            <p>6 month intensive software engineering course focused on: HTML, CSS, JavaScript/React and Ruby (Rails). (40 hours weekly, 5 days per week).</p>
-            <p>Complete daily projects with assigned partner. Developed strong collaboration skills with fellow junior engineers while working through difficult problems.</p>
-            <p>Completed several group projects with engineering team. We collectively: Determined product roadmap, assigned responsibilities, and successfully merged our work together into finished applications.</p>
-            <p>Highly focused on leveraging Google and figuring out problems for myself before asking a colleague. Broadened conceptual understanding and solved difficult problems with this approach.</p>
+            <h5>Code for PDX</h5>
+            <p className="italic">Open Source Contributor, 2019-Present</p>
+            <p>Philanthropic programming organization focused on designing, building and maintaining software for social organizations that need it.</p>
+            <p>Find/fix bugs and build new features. Communicate effectively between myself and the PM to understand the best possible solution for the client.</p>
+            <p>Collaborate effectively with team of Senior Engineers. We collectively: determine product roadmap, assign responsibilities, and successfully merge our work together into finished applications.</p>
+            <p>Highly focused on leveraging Google and figuring out problems for myself before asking a colleague. I broaden my conceptual understanding and solve difficult problems with this approach.</p>
+            <p ><a className="linkStyle" href="https://github.com/codeforpdx/dwellingly-app">Check out our current project and my commits here!</a></p>
 
 
             <h5>ASKNICELY</h5>
             <p className="italic">Account Executive 2017-2019</p>
-            <p>Frequently met with customers and VP of Product to discuss roadmap prioritization. Assisted our leadership in making sure the highest ROI features get delivered first</p>
             <p>Top performing Account Executive on a team of 15. Q3 2019.</p>
+            <p>Developed strong communication skills. Deeply understand the 'why' and business impact of a new feature before bringing it to the PM team.</p>
             <p>Use consultative selling approach to expand on current accounts, and own full sales cycle in an effort to earn new business for the organization.</p>
             <p>Heavy focus on ‘Player-Coach’ mentality. While holding quota was also responsible for training new Account Executives, advising Business Development team, and working with Customer Success on Up-sells.</p>
 
